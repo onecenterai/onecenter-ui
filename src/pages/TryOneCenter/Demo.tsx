14742f76b7f63530bdf3d3f4119afa6da9be8a66
Ibrahim <i.aliyu019@gmail.com>
2023-10-14 12:33:28 +0100

    cleaned demo.tsx

--- src/pages/TryOneCenter/Demo.tsx
+++ src/pages/TryOneCenter/Demo.tsx
@@ -1,13 +1,7 @@
-import Drawer from "./Drawer";
 import Card from "./Card";
 import { Box, Container, Grid } from "@mui/material";
-// import { ReactSipPhone } from "react-sip-phone";
 import "react-sip-phone/dist/index.css";
-import { StyledInput } from "../../styled-components/styledInput";
-import { ClassNames } from "@emotion/react";
 import Search from "../../components/Search";
-// import { useDispatch } from "react-redux";
-import { EngageCallFunctions } from "../../components/Engage/EngageCallFunctions";
 import EngageCall from "../../components/Engage/EngageCall";
 
 function Demo() {
